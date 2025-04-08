86c16efc5d20c27b5e62216ecf6a5e3e5ead9822
cj4mm <47518865+cj4mm@users.noreply.github.com>
2025-04-09 08:14:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,5 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: "dist", // ✅ Vercel이 찾을 수 있도록 빌드 출력 경로 명시
   },
 });
