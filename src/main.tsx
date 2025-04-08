95ac10b8f9d28b16f9197e3e2b2cb643db3b8cd2
cj4mm <junsoo511@naver.com>
2025-04-08 17:16:11 +0900

    🧠 프로젝트 구조 재설정 후 최초 업로드

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AsciiXOREncryptor from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AsciiXOREncryptor />
+  </React.StrictMode>
+);
